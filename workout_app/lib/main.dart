084669fb395b2654cf5dbb68ada199f5123c27cc
OnceNamedSam <202195592@collegeahuntsic.qc.ca>
2021-07-01 11:59:15 -0400

    Project ready to be team-tested with card coding

--- workout_app/lib/main.dart
+++ workout_app/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import './app_ecrans/ecran_depart.dart';
+import './app_ecrans/sams_card.dart';
+import './app_ecrans/siarheis_card.dart';
+import './app_ecrans/yulias_card.dart';
 
 // Ouvrir un emulateur dans Android Studio, click Run ou Ctrl+F5
 void main() => runApp(MyWorkoutApp());
@@ -16,7 +19,14 @@ class MyWorkoutApp extends StatelessWidget {
             // ignore: prefer_const_constructors
             title: Text('WORKOUT APP'),
           ),
-          body: EcranDepart(), //Crée une instance de EcranDepart
+          body: Column(
+            children: <Widget>[
+              EcranDepart(), //Crée une instance de EcranDepart
+              SamsCard(),
+              SiarheisCard(),
+              YuliasCard()
+            ]
+          )
         ),
       ),
     );
