daf873f7b145d4f92c10a04f23afb790ab07b8b9
OnceNamedSam <202195592@collegeahuntsic.qc.ca>
2021-07-12 10:46:49 -0400

    Sams cards added

--- workout_app/lib/app_ecrans/sams_card.dart
+++ workout_app/lib/app_ecrans/sams_card.dart
@@ -5,94 +5,111 @@ class SamsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
-    return Container(
-      height: 220,
-      width: double.maxFinite,
-      padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-      child: Row(
-        children: <Widget>[firstCard, secondCard],
+    return Expanded(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xff2e3338),
+              Color(0xff111215),
+            ],
+          ),
+        ),
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+        child: GridView.count(
+          shrinkWrap: true,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          crossAxisCount: 2,
+          children: generateCards(),
+        ),
       ),
     );
   }
 }
 
-final firstCard = Expanded(
-  child: Card(
-    clipBehavior: Clip.antiAliasWithSaveLayer,
-    child: Column(
-      children: [
-        SizedBox(
-          height: 165,
-          child: Image.asset(
-            'images/img1.jpg',
-            fit: BoxFit.cover,
-          ),
-        ),
-        const Spacer(),
-        const Text(
-          'Flexibilité',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontFamily: "Rubik"),
-        ),
-        const Spacer(),
-      ],
-    ),
-    margin: const EdgeInsets.all(5.0),
-    elevation: 5,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(3),
-    ),
-  ),
-);
+var donnees = [
+  {'titre': 'Biceps', 'image': 'images/musculation.jpg'},
+  {'titre': 'Endurance', 'image': 'images/endurance.jpg'},
+  {'titre': 'Perte de poids', 'image': 'images/perte_de_poids.jpg'},
+  {'titre': 'Puissance', 'image': 'images/puissance.jpg'},
+  {'titre': 'Flexibilité', 'image': 'images/flexibilite.jpg'},
+  {'titre': 'Cardio', 'image': 'images/cardio.jpg'},
+  {'titre': 'Maintient', 'image': 'images/garder_la_forme.jpg'},
+  {'titre': 'Abdominaux', 'image': 'images/abdominaux.jpg'}
+];
 
-final secondCard = Expanded(
-  child: Card(
-    clipBehavior: Clip.antiAliasWithSaveLayer,
-    child: Column(
-      children: [
-        SizedBox(
-          height: 165,
-          child: Image.asset(
-            'images/img2.jpg',
-            fit: BoxFit.cover,
+List<Widget> generateCards() {
+  List<Widget> gymCards = <Widget>[];
+
+  donnees.forEach((workoutGoal) => gymCards.add(Card(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        margin: const EdgeInsets.all(5.0),
+        elevation: 6,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color(0xff2e3338),
+                Color(0xff111215),
+              ],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                //height: 145,
+                child: Image.asset(
+                  workoutGoal['image']!,
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+              const Spacer(),
+              Stack(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        left: 18,
+                      ),
+                      child: Text(
+                        workoutGoal['titre']!,
+                        style: const TextStyle(
+                          fontFamily: "Roboto",
+                          color: Color(0xffafb1b3),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        right: 13,
+                      ),
+                      child: Icon(
+                        Icons.play_circle_fill,
+                        color: Colors.pink,
+                        size: 28.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+            ],
           ),
         ),
-        const Spacer(),
-        Stack(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: <Widget>[
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.only(
-                  left: 15,
-                ),
-                child: Text('Puissance', style: TextStyle(fontFamily: "Roboto", fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            const Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.only(
-                  right: 10,
-                ),
-                child: Icon(
-                  Icons.fitness_center_rounded,
-                  color: Colors.black,
-                  size: 18.0,
-                ),
-              ),
-            ),
-          ],
-        ),
-        const Spacer(),
-      ],
-    ),
-    margin: const EdgeInsets.all(5.0),
-    elevation: 5,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(18),
-    ),
-  ),
-);
+      )));
+  return gymCards;
+}
